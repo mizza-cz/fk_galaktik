c80cc9acd3b63ba717451ae4e5d6ad4293d0a40b
mizza-cz <mizza.cz666@gmail.com>
2022-10-19 10:57:53 +0200

    ad hp

---
+++ src/components/partners/partners.js
@@ -0,0 +1,41 @@
+$(".js-slider").slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  arrows: true,
+  autoplay: true,
+  infinite: true,
+  prevArrow:
+    '<button class="slider__btn slider__btnprev"><img src="images/ico/left.svg" alt="" ></button> ',
+  nextArrow:
+    ' <button class="slider__btn  slider__btnnext"><img src="images/ico/right.svg" alt = "" ></button>',
+  responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 5,
+        arrows: false,
+      },
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        arrows: false,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 3,
+        arrows: false,
+      },
+    },
+    {
+      breakpoint: 420,
+      settings: {
+        slidesToShow: 2,
+        arrows: false,
+      },
+    },
+  ],
+});
