af3f32ace0176f7ab3e08b6005d2d6ca19f5c367
mizza-cz <mizza.cz666@gmail.com>
2025-11-06 13:07:39 +0100

    add slider pubs

---
+++ src/js/custom/pub-slider.js
@@ -0,0 +1,39 @@
+$(".pub-slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+  infinite: true,
+  customPaging: "20px",
+
+  responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 2,
+        arrows: false,
+      },
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        arrows: false,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        arrows: false,
+      },
+    },
+    {
+      breakpoint: 420,
+      settings: {
+        slidesToShow: 1,
+        arrows: false,
+      },
+    },
+  ],
+});
